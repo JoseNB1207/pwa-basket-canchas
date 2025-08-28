4b8998348a5d97c6c83b232d5bdf79c156a2fb05
JoseNB1207 <jose.ninob@uniagustiniana.edu.co>
2025-08-28 14:56:07 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'canchas-basket-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',        // asumiendo que tu html se llama así
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://cdn-icons-png.flaticon.com/256/10443/10443831.png',
+  // Agrega aquí otros recursos estáticos que uses (css, js, imágenes)
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
